0ed195e9b64048839f3575e62ea38a97901f59bb
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-05-12 09:16:10 +0700

    Wire attendance overtime schema extension

--- cohan-restaurant-backend/graphql/schema/index.js
+++ cohan-restaurant-backend/graphql/schema/index.js
@@ -38,6 +38,7 @@ const files = [
   "communication.graphql",
   "availability.graphql",
   "posCustomer.graphql",
+  "attendance_overtime.graphql",
 ].map((f) => fs.readFileSync(path.join(schemaDir, f), "utf8"));
 
 const typeDefs = mergeTypeDefs(files, { useSchemaDefinition: true });
