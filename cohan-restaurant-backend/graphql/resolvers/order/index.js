b1b3bc19b864263851afc4ac4db7d782b5d33156
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-05-12 13:40:02 +0700

    Wire public table session query resolver

--- cohan-restaurant-backend/graphql/resolvers/order/index.js
+++ cohan-restaurant-backend/graphql/resolvers/order/index.js
@@ -6,6 +6,7 @@ import { withOrderRestaurantAccessGuards } from "./accessGuard.js";
 import { OrderResolvers } from "./types.js";
 import { OrderSubscription } from "./subscription.js";
 import { withTablePaymentRequestLifecycle } from "./tablePaymentRequestLifecycle.js";
+import publicTableSessionQuery from "./publicTableSessionQuery.js";
 
 const LifecycleOrderMutation = withTablePaymentRequestLifecycle(OrderMutation);
 const GuardedOrderMutation = withOrderRestaurantAccessGuards(LifecycleOrderMutation);
@@ -13,6 +14,7 @@ const GuardedOrderMutation = withOrderRestaurantAccessGuards(LifecycleOrderMutat
 export default {
   Query: {
     ...OrderQuery,
+    ...publicTableSessionQuery,
   },
   Mutation: {
     ...GuardedOrderMutation,
