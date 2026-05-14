9f0412de36a07dca64f2a0673b257d733192d31b
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-05-15 02:29:08 +0700

    Import availability snapshot polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import "./styles/schedule-manager-experience.css";
 import "./styles/schedule-action-center.css";
 import "./styles/schedule-auto-modal-polish.css";
 import "./styles/schedule-rules-modal-polish.css";
+import "./styles/availability-snapshot-polish.css";
 import { initFrontendErrorTracking } from "./observability/errorTracking.js";
 import { initScheduleManagerDomPolish } from "./utils/scheduleManagerDomPolish.js";
 
