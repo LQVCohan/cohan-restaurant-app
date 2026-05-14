18b698f9a128d4f7e1f94103a64969bfd3bd58eb
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-05-15 02:46:26 +0700

    Import shift detail modal polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import "./styles/schedule-action-center.css";
 import "./styles/schedule-auto-modal-polish.css";
 import "./styles/schedule-rules-modal-polish.css";
 import "./styles/availability-snapshot-polish.css";
+import "./styles/shift-detail-polish.css";
 import { initFrontendErrorTracking } from "./observability/errorTracking.js";
 import { initScheduleManagerDomPolish } from "./utils/scheduleManagerDomPolish.js";
 
