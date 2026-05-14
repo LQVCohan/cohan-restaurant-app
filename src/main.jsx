8aee58458c07da431c50f525baecd9b673dd61b3
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-05-14 10:55:20 +0700

    Import schedule manager experience styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./styles/schedule-manager-experience.css";
 import App from "./App.jsx";
 import { initFrontendErrorTracking } from "./observability/errorTracking.js";
 
