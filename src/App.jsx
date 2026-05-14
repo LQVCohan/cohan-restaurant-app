e262a0ae8fa450434ecc78aae51ed737e57a607a
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-05-15 03:51:32 +0700

    Load schedule insights polish from app entry

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { AuthProvider } from "./context/AuthProvider";
 import { ApolloProvider } from "@apollo/client/react";
 import { apolloClient } from "./apollo/client";
 import "./styles/Globals.scss";
+import "./styles/schedule-insights-polish.css";
 import ScrollToTop from "./components/common/ScrollToTop";
 import NotificationContainer from "./components/common/NotificationContainer";
 import NotificationProvider from "./context/NotificationProvider";
