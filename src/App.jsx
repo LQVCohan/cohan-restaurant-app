39e16017aeedffdf5672aed824e5c684d3796d4f
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-05-15 04:53:28 +0700

    Load schedule publish polish styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import "./styles/Globals.scss";
 import "./styles/schedule-insights-polish.css";
 import "./styles/add-shift-modal-polish.css";
 import "./styles/schedule-board-polish.css";
+import "./styles/schedule-publish-polish.css";
 import ScrollToTop from "./components/common/ScrollToTop";
 import NotificationContainer from "./components/common/NotificationContainer";
 import NotificationProvider from "./context/NotificationProvider";
