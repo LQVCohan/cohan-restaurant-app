62e06ef029ed49c5167bdacfce6b545dc693b3e9
Cohan Le <vietle632@gmail.com>
2026-05-14 22:15:23 +0700

    Initialize schedule manager DOM polish and import action center styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "./styles/schedule-manager-experience.css";
+import "./styles/schedule-action-center.css";
 import { initFrontendErrorTracking } from "./observability/errorTracking.js";
+import { initScheduleManagerDomPolish } from "./utils/scheduleManagerDomPolish.js";
 
 void initFrontendErrorTracking();
+initScheduleManagerDomPolish();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
