128fe65ea04d5a5c4eaaba515f551830768b8e65
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-05-15 07:00:28 +0700

    Add schedule page wrapper for local polish styles

---
+++ src/components/Dashboard_Manager/Schedule/ScheduleManagementPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "@/styles/schedule-polish.css";
+import ScheduleManagement from "./ScheduleManagement";
+
+const ScheduleManagementPage = () => <ScheduleManagement />;
+
+export default ScheduleManagementPage;
