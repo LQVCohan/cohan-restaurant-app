afe0901168ad1f56c6cf89f3758a70dbc1f7a39f
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-05-18 00:04:37 +0700

    Wire safe reservation check-in resolver

--- cohan-restaurant-backend/graphql/resolvers/reservation/index.js
+++ cohan-restaurant-backend/graphql/resolvers/reservation/index.js
@@ -3,13 +3,24 @@
 import { ReservationQuery } from "./query.js";
 import { ReservationMutation } from "./mutation.js";
 import { ReservationChangeReviewMutation } from "./changeReview.js";
+import {
+  ReservationCheckInMutation,
+  withSafeReservationStatusMutation,
+} from "./checkIn.js";
+
+const ReviewReservationMutation = {
+  ...ReservationMutation,
+  ...ReservationChangeReviewMutation,
+  ...ReservationCheckInMutation,
+};
+
+const SafeReservationMutation = withSafeReservationStatusMutation(ReviewReservationMutation);
 
 export default {
   Query: {
     ...ReservationQuery,
   },
   Mutation: {
-    ...ReservationMutation,
-    ...ReservationChangeReviewMutation,
+    ...SafeReservationMutation,
   },
 };
