9faac2ecc248852981d29ad2f9a9ebf4802d8608
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-10 04:06:31 +0700

    style: restore and upgrade AI chatbot knowledge management UI

--- src/components/Dashboard_Manager/Customer/AiChatbotKnowledgePage.jsx
+++ src/components/Dashboard_Manager/Customer/AiChatbotKnowledgePage.jsx
@@ -1,6 +1,7 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useMemo, useState } from "react";
 import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client";
 import { AuthContext } from "@/context/AuthContext";
 import "./AiChatbotAdmin.scss";
 
-const Q = gql`query ManagerAiKnowledge($restaurantId: ID!, $filter: AiChatbotKnowledgeFilterInput) { restaurantAiChatbotKnowledge(restaurantId: $restaurantId, filter
+const Q = gql`query ManagerAiKnowledge($restaurantId: ID!, $filter: AiChatbotKnowledgeFilterInput) { restaurantAiChatbotKnowledge(restaurantId: $restaurantId, filter: $filter) { id title content category tags enabled priority sourceType } }`;
+const Q_SUG
