1f12ba8f53b781c6e301a2899aae6f1817c8d516
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-10 01:03:29 +0700

    Load kitchen display polish behavior

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./components/Customer/Food/FoodDetail.polish.css";
 import "./components/Dashboard_Manager/Order/orderKitchenPolish.css";
+import "./components/Dashboard_Manager/Order/orderKitchenPolish.js";
 import App from "./App.jsx";
 import { initFrontendErrorTracking } from "./observability/errorTracking.js";
 import { installAuthenticatedTable3DTransport } from "@/lib/installAuthenticatedTable3DTransport";
