09bd4b20bd73270866cd19097d9b2d62ad8281ff
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-10 05:33:20 +0700

    fix: complete AI chatbot knowledge page

--- src/components/Dashboard_Manager/Customer/AiChatbotKnowledgePage.jsx
+++ src/components/Dashboard_Manager/Customer/AiChatbotKnowledgePage.jsx
@@ -1,6 +1,6 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useMemo, useState } from "react";
 import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client";
 import { AuthContext } from "@/context/AuthContext";
 import "./AiChatbotAdmin.scss";
 
-const Q = gql`query ManagerAiKnowledge($restaurantId: ID!, $filter: AiChatbotKnowledgeFilterInput) { restaurantAiChatbotKnowledge(restaurantId: $restaurantId, filter: $filter) { id title content category tags enabled priority
+const Q = gql`query ManagerAiKnowledge($restaurantId: ID!, $filter: AiChatbotKnowledgeFilterInput) { restaurantAiChatbotKnowledge(restaurantId: $restaurantId
