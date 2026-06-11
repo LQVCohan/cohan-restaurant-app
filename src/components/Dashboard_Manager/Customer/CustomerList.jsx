fffcfb8ad68747777093b7b936078c45c933e6f3
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-12 01:44:20 +0700

    Restore customer list component safely

--- src/components/Dashboard_Manager/Customer/CustomerList.jsx
+++ src/components/Dashboard_Manager/Customer/CustomerList.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { SearchX } from "lucide-react";
 import CustomerCard from "./CustomerCard";
 import "./CustomerList.scss";
-import "./CustomerWorkspacePolish.scss";
 
 const CustomerList = ({ customers, loading, onCustomerClick }) => {
   if (loading) {
-    return <div className="cl-grid">{Array.from({ length: 6 }).map((_, i) => <div key={i} className="cl-skeleton-card"><div className="cl-sk-header"><
+    return React.createElement(
+      "div",
+      { className: "cl-grid" },
+      Array.from({ length: 
