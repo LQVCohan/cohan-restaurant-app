e8d1c23ccb8f1251b3ec616861291e1f414bda4a
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-11 18:51:14 +0700

    Load kitchen fullscreen override

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,4 @@ import "./components/Customer/Food/FoodDetail.polish.css";
 import "./components/Dashboard_Manager/Order/orderKitchenPolish.css";
 import "./components/Dashboard_Manager/Order/orderKitchenStatusFilter.css";
 import "./components/Dashboard_Manager/Order/orderManagementCompactLayout.css";
-import "./components/Dashboard_Manager/Order/orderKitchenPolish.js";
-import App from "./App.jsx";
-import { initFrontendErrorTracking } from "./observability/errorTracking.js";
-import { installAuthenticatedTable3DTransport } from "@/lib/installAuthenticatedTable3DTransport";
-
-void initFrontendErrorTracking();
-installAuthenticatedTable3DTransport();
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+import "
