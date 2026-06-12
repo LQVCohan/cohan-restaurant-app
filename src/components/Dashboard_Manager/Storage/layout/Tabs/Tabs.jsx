66f6fb9e16d7c97ce67e221ee7e55720c6ee1cb2
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-13 00:04:39 +0700

    Load premium storage polish

--- src/components/Dashboard_Manager/Storage/layout/Tabs/Tabs.jsx
+++ src/components/Dashboard_Manager/Storage/layout/Tabs/Tabs.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./Tabs.scss";
 import "../../StorageExperiencePolish.css";
 import "../../StorageBackgroundUnify.css";
+import "../../StoragePremiumNine.css";
 
 const Tabs = ({ tabs, activeTab, onTabChange }) => {
   return (
