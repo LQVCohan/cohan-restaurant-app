bcf27cb6fd33438320a856c0d393fc42f34cf426
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-14 02:16:13 +0700

    Load manager typography override globally

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./components/Customer/Food/FoodDetail.polish.css";
+import "./styles/ManagerTypography.css";
 
 import App from "./App.jsx";
 import { initFrontendErrorTracking } from "./observability/errorTracking.js";
