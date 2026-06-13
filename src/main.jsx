197a1fda13e630845e49a296cfe632ded6f9a428
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-14 02:18:10 +0700

    Load final manager typography system override

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./components/Customer/Food/FoodDetail.polish.css";
 import "./styles/ManagerTypography.css";
+import "./styles/ManagerTypographySystemOverride.css";
 
 import App from "./App.jsx";
 import { initFrontendErrorTracking } from "./observability/errorTracking.js";
