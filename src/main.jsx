5ac41aaa9941385a3ceef9f945e9677153d962d5
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-18 23:02:12 +0700

    Load compact RBAC layout polish

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import "./components/common/AiChatbotWidgetInlineSuggestions.css";
 
 import App from "./App.jsx";
 import "./styles/RbacPayrollTheme.css";
+import "./styles/RbacCompactLayout.css";
 import { initFrontendErrorTracking } from "./observability/errorTracking.js";
 import { installAuthenticatedTable3DTransport } from "@/lib/installAuthenticatedTable3DTransport";
 
