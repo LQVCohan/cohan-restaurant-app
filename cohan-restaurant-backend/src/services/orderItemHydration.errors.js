a9e950b4a9234f6afaaecc5a6a51787a114b2549
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-22 04:01:55 +0700

    Define hydration error details

--- cohan-restaurant-backend/src/services/orderItemHydration.errors.js
+++ cohan-restaurant-backend/src/services/orderItemHydration.errors.js
@@ -1,1 +1,7 @@
-export class OrderItemHydrationError extends Error {}
+export class OrderItemHydrationError extends Error {
+  constructor(message, code = "INVALID_ITEMS") {
+    super(message);
+    this.name = "OrderItemHydrationError";
+    this.code = code;
+  }
+}
