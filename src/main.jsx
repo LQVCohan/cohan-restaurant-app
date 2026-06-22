b1de2d392d9217b0fa6e840a508fefeb47a8ceca
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-22 18:32:48 +0700

    Import camera preview repair styles

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,7 @@ import "./styles/Table3DModalHeaderCompact.css";
 import "./styles/Table3DModalWorkflow.css";
 import "./styles/TableARNestedModals.css";
 import "./styles/Table3DToolbarMobileFix.css";
+import "./styles/TableCameraPreviewRepair.css";
 import "./components/common/AiChatbotWidgetInlineSuggestions.css";
 
 import App from "./App.jsx";
