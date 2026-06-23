3db11d3c8ec7b5e76da4e6bd7eadae18ab4bc6aa
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-24 05:05:57 +0700

    Wire guarded payment mutations

--- cohan-restaurant-backend/graphql/resolvers/payment/index.js
+++ cohan-restaurant-backend/graphql/resolvers/payment/index.js
@@ -2,6 +2,8 @@ import { PaymentQuery } from "./query.js";
 import { BankTransferPaymentQuery } from "./bankTransferQuery.js";
 import PaymentMutation from "./mutation.js";
 import ReconciliationPaymentConfirmationMutation from "./reconciliationPaymentConfirmationMutation.js";
+import FinanceOperationGuardMutation from "./financeOperationGuardMutation.js";
+import StrictOrderPaymentMutation from "./strictOrderPaymentMutation.js";
 import TransferPaymentMutation from "./transferMutation.js";
 import { PaymentResolvers } from "./types.js";
 import publicTablePaymentMutation from "./publicTablePaymentMutation.js";
@@ -14,6 +16,8 @@ export default {
   Mutation: {
     ...PaymentMutation,
     ...ReconciliationPaymentConfirmationMutation,
+    ...FinanceOperationGuardMutation,
+    ...StrictOrderPaymentMutation,
     ...TransferPaymentMutation,
     ...publicTablePaymentMutation,
   },
