a8dd2550eb1c7bfa095c21effc07cc3f383f10de
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-24 02:15:21 +0700

    Wire reconciliation payment confirmation mutation

--- cohan-restaurant-backend/graphql/resolvers/payment/index.js
+++ cohan-restaurant-backend/graphql/resolvers/payment/index.js
@@ -1,6 +1,7 @@
 import { PaymentQuery } from "./query.js";
 import { BankTransferPaymentQuery } from "./bankTransferQuery.js";
 import PaymentMutation from "./mutation.js";
+import ReconciliationPaymentConfirmationMutation from "./reconciliationPaymentConfirmationMutation.js";
 import TransferPaymentMutation from "./transferMutation.js";
 import { PaymentResolvers } from "./types.js";
 import publicTablePaymentMutation from "./publicTablePaymentMutation.js";
@@ -12,6 +13,7 @@ export default {
   },
   Mutation: {
     ...PaymentMutation,
+    ...ReconciliationPaymentConfirmationMutation,
     ...TransferPaymentMutation,
     ...publicTablePaymentMutation,
   },
