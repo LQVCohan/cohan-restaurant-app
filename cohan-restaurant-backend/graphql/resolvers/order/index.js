e77aac624a507caeab5fdf9c313b5fe889fe9443
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-25 05:42:11 +0700

    Use guarded customer tracking payment mutation

--- cohan-restaurant-backend/graphql/resolvers/order/index.js
+++ cohan-restaurant-backend/graphql/resolvers/order/index.js
@@ -3,6 +3,7 @@
 import { OrderQuery } from "./query.js";
 import { OrderCoreRecoveryQuery } from "./queryCoreRecovery.js";
 import { OrderMutation } from "./mutation.js";
+import { CustomerTrackingPaymentMutation } from "./customerTrackingPaymentMutation.js";
 import { OrderProofMutation } from "./orderProofMutation.js";
 import { withOrderRestaurantAccessGuards } from "./accessGuard.js";
 import { OrderResolvers } from "./types.js";
@@ -12,7 +13,11 @@ import { withOrderConflictHardening } from "./orderConflictHardening.js";
 import { withCheckoutIdempotency } from "./checkoutIdempotency.js";
 import publicTableSessionQuery from "./publicTableSessionQuery.js";
 
-const LifecycleOrderMutation = withTablePaymentRequestLifecycle(OrderMutation);
+const PaymentGuardedOrderMutation = {
+  ...OrderMutation,
+  ...CustomerTrackingPaymentMutation,
+};
+const LifecycleOrderMutation = withTablePaymentRequestLifecycle(PaymentGuardedOrderMutation);
 const HardenedOrderMutation = withOrderConflictHardening(LifecycleOrderMutation);
 const CheckoutSafeOrderMutation = withCheckoutIdempotency(HardenedOrderMutation);
 const GuardedOrderMutation = withOrderRestaurantAccessGuards(CheckoutSafeOrderMutation);
