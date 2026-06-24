a0c3f3b1a1b6e535bbc8b33fb7aaa6b47ed01dbd
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-24 10:29:09 +0700

    Load AR mobile test polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -35,6 +35,7 @@ import "./components/Dashboard_Manager/Table/TableManagementPolish.scss";
 import "./components/Dashboard_Manager/Table/TableManagementFinalQC.scss";
 import "./components/Dashboard_Manager/Table/TableManagementScorePolish.scss";
 import "./components/Dashboard_Manager/Table/TableWorkflowModalPremium.scss";
+import "./components/Dashboard_Manager/Table/TableMobileARTestPolish.scss";
 import "./components/Dashboard_Manager/Table/FloorPlanDesignerExperience.scss";
 import "./components/Dashboard_Manager/RestaurantInfo/RestaurantInfoManagementPolish.scss";
 import "./components/Dashboard_Manager/RestaurantInfo/RestaurantInfoToneSync.scss";
