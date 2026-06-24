a78af5c152a878701078994d98c2948a98825cfa
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-24 22:28:55 +0700

    Load table modal observation UX

--- src/main.jsx
+++ src/main.jsx
@@ -37,6 +37,7 @@ import "./components/Dashboard_Manager/Table/TableManagementScorePolish.scss";
 import "./components/Dashboard_Manager/Table/TableWorkflowModalPremium.scss";
 import "./components/Dashboard_Manager/Table/TableWorkflowFinalPolish.scss";
 import "./components/Dashboard_Manager/Table/TableWorkflowModalOverlapFix.scss";
+import "./components/Dashboard_Manager/Table/TableWorkflowObservationUX.scss";
 import "./components/Dashboard_Manager/Table/TableMobileARTestPolish.scss";
 import "./components/Dashboard_Manager/Table/FloorPlanDesignerExperience.scss";
 import "./components/Dashboard_Manager/RestaurantInfo/RestaurantInfoManagementPolish.scss";
