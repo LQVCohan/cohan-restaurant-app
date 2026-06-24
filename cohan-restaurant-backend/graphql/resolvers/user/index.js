ec78a045e54422125d142f89a26f1a61f0462dac
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-24 23:20:47 +0700

    Override login resolver for pending verification

--- cohan-restaurant-backend/graphql/resolvers/user/index.js
+++ cohan-restaurant-backend/graphql/resolvers/user/index.js
@@ -1,8 +1,10 @@
 import { UserQuery } from "./query.js";
 import { UserMutation } from "./mutation.js";
+import { loginWithPendingVerification } from "./loginWithPendingVerification.mutation.js";
 import User from "./types.js";
+
 export default {
   Query: { ...UserQuery },
-  Mutation: { ...UserMutation },
+  Mutation: { ...UserMutation, login: loginWithPendingVerification },
   ...User,
 };
