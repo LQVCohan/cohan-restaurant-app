aeae897a2f588de3ffaafe2594fbddac3bc390ee
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-25 23:39:54 +0700

    Ensure modal styles override staff leave theme

--- src/components/Staff/StaffLeavePage.jsx
+++ src/components/Staff/StaffLeavePage.jsx
@@ -4,8 +4,8 @@ import LeaveRequestForm from "@/components/Dashboard_Manager/Staff/components/Le
 import LeaveRequestsList from "@/components/Dashboard_Manager/Staff/components/LeaveManagement/LeaveRequestsList";
 import { useLeaveManagement } from "@/hooks/useLeaveManagement";
 import "@/components/Dashboard_Manager/Staff/components/LeaveManagement/LeaveManagement.scss";
-import "@/components/Dashboard_Manager/Staff/components/LeaveManagement/LeaveModal.scss";
 import "./StaffLeavePage.scss";
+import "@/components/Dashboard_Manager/Staff/components/LeaveManagement/LeaveModal.scss";
 
 const resolveId = (value) => {
   if (!value) return "";
