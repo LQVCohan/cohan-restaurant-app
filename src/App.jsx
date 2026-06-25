e51e1947ec5b6e979304492b017b4c5ce0c35dcd
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-26 00:25:16 +0700

    Load For You typography polish

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { apolloClient } from "./apollo/client";
 import "./styles/Globals.scss";
 import "./components/LoginPolish.scss";
 import "./components/LoginAudiencePolish.scss";
+import "./components/Customer/ForYou/ForYouTypographyPolish.scss";
 import "./components/Customer/RestaurantMenu/styles/MenuDetailViewPolish.scss";
 import ScrollToTop from "./components/common/ScrollToTop";
 import NotificationContainer from "./components/common/NotificationContainer";
