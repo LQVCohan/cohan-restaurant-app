c78118c7ddba613c660a0a13535787c99bc5871f
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-26 16:41:09 +0700

    Add customer coupon wallet route

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -214,6 +214,7 @@ const AppRouter = () => (
       <Route path="/checkout" element={withPrivateRoute(<CheckoutPage />, ["customer"])} />
       <Route path="/food/:foodId" element={<FoodDetail />} />
       <Route path="/vouchers/:id" element={<Navigate to="/restaurants" replace />} />
+      <Route path="/coupons" element={withPrivateRoute(<CouponPage />, ["customer", "manager", "admin"])} />
       <Route path="/coupons/:restaurantId" element={<CouponPage />} />
       <Route path="/favorites/:id" element={withPrivateRoute(<FavoritePage />, ["customer", "manager", "admin"])} />
       <Route path="/address-book/:id" element={withPrivateRoute(<AddressPage />, ["customer", "manager", "admin"])} />
