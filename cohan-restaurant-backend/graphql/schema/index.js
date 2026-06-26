b7ee25ef92d4a56fd7e9ec09dd8c2213b6c8fcb9
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-26 07:33:43 +0700

    Load reservation change review schema

--- cohan-restaurant-backend/graphql/schema/index.js
+++ cohan-restaurant-backend/graphql/schema/index.js
@@ -24,6 +24,7 @@ const files = [
   "inventory.graphql",
   "order.graphql",
   "reservation.graphql",
+  "reservationChangeReview.graphql",
   "supply.graphql",
   "event_log.graphql",
   "payments.graphql",
