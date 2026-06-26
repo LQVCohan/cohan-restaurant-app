857aa50e936cb656a1454b20f41338bef55a00e5
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-06-26 16:47:32 +0700

    Wire table QR access query

--- cohan-restaurant-backend/graphql/resolvers/table/index.js
+++ cohan-restaurant-backend/graphql/resolvers/table/index.js
@@ -1,13 +1,14 @@
 import tableQueries from "./query.js";
 import tableMutations from "./mutation.js";
 import { CustomerPublicTableMutation } from "./publicCustomer.js";
-import TableAccessQrMutation from "./tableAccessQr.js";
+import TableAccessQrMutation, { TableAccessQrQuery } from "./tableAccessQr.js";
 
 import { TableCustomerQuery, TableCustomerMutation } from "./tableCustomer.js";
 
 export default {
   Query: {
     ...tableQueries,
+    ...TableAccessQrQuery,
 
     ...TableCustomerQuery, // ✅ thêm
   },
