2b42ca12b88453e3c62f31e6de1db4f78ea50e34
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-03 00:59:42 +0700

    Load approved AI Chef artwork

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./styles/AiChefConciergeGenerated.css";
 import "./components/Customer/Food/FoodDetail.polish.css";
 import "./styles/ManagerTypography.css";
 import "./styles/ManagerTypographySystemOverride.css";
