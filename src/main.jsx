7adef80af00b1bde3ba7f6522a1efbaaba54cabe
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-03 11:43:32 +0700

    load manager dark mode after all manager page styles

--- src/main.jsx
+++ src/main.jsx
@@ -45,6 +45,7 @@ import "./styles/CohanBrandIdentity.css";
 import "./styles/AiChatbotRoleAvatars.css";
 import "./layouts/ManagerSageSurfaceOverrides.css";
 import "./styles/AttendanceManagerVisualFix.css";
+import "./styles/ManagerDarkModeFinal.scss";
 import { applyChatThreadToolbarStyle } from "./utils/chatThreadToolbarStyle";
 import { installAiChatbotStreamFetchPatch } from "./utils/aiChatbotStreamFetchPatch";
 
