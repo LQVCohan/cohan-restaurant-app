d90d4e1f0672cee9a4a1f30b4d52f1072efccff5
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-06 23:52:39 +0700

    test: provide browser layout APIs for component tests

--- src/test/setup.js
+++ src/test/setup.js
@@ -2,3 +2,26 @@ import '@testing-library/jest-dom/vitest';
 import { vi } from 'vitest';
 
 vi.stubEnv('VITE_API_URL', 'http://localhost:4000/graphql');
+
+class ResizeObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+vi.stubGlobal('ResizeObserver', ResizeObserverMock);
+
+Object.defineProperty(window, 'matchMedia', {
+  configurable: true,
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
