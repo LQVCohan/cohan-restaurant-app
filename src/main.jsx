02df4c3acfc4d698e6002f83363cb782ade15b5d
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-07 14:40:04 +0700

    style: load table mobile action visibility fix last

--- src/main.jsx
+++ src/main.jsx
@@ -49,6 +49,7 @@ import "./components/Dashboard_Manager/Account/ManagerAccountCenterPolish.scss";
 import "./styles/ManagerDarkModeFinal.scss";
 import "./styles/MobileAuthTableExperience.css";
 import "./styles/ManagerMobileRefinement.css";
+import "./styles/TableManagerMobileActionFix.css";
 import { applyChatThreadToolbarStyle } from "./utils/chatThreadToolbarStyle";
 import { installRbacVietnameseLabels } from "./utils/rbacVietnameseLabels";
 
