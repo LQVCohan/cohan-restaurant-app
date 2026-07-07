10606ec9363e4103e808a2d4223d4de9974f9f70
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-08 03:21:19 +0700

    style: load restaurant preview polish

--- src/main.jsx
+++ src/main.jsx
@@ -55,6 +55,7 @@ import "./styles/TableManagerMobileActionFix.css";
 import "./styles/RestaurantProfileHoursResponsiveFix.css";
 import "./components/Dashboard_Manager/RestaurantInfo/RestaurantInfoUnifiedPage.css";
 import "./components/Dashboard_Manager/RestaurantInfo/RestaurantInfoPremiumLayout.css";
+import "./components/Dashboard_Manager/RestaurantInfo/RestaurantPreviewPolish.css";
 import "./components/Dashboard_Manager/Staff/components/Attendance/OvertimePanelPolicyPolish.scss";
 import "./components/Dashboard_Manager/Table/TableManagementCanvasSync.css";
 import "./components/Dashboard_Manager/Table/TableManagementMergePickerFix.css";
