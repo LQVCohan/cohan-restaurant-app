670b83a0f0b05c7d5aee7d1c588cf8ea4ff0b5ab
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-08 11:22:33 +0700

    feat(table): resolve composite table details

--- cohan-restaurant-backend/graphql/resolvers/table/index.js
+++ cohan-restaurant-backend/graphql/resolvers/table/index.js
@@ -1,6 +1,7 @@
 import tableQueries from "./query.js";
 import tableMutations from "./mutation.js";
 import mergeTableMutations from "./mergeTables.js";
+import { resolveTableMergeDetails } from "./mergeDetails.js";
 import moveTable from "./moveTable.js";
 import { CustomerPublicTableMutation } from "./publicCustomer.js";
 import TableAccessQrMutation, { TableAccessQrQuery } from "./tableAccessQr.js";
@@ -27,6 +28,7 @@ export default {
     viewLockUserId: (p) => p?.viewLock?.userId || null,
     viewLockExpiresAt: (p) => p?.viewLock?.expiresAt || null,
     viewLockViewerName: (p) => p?.viewLock?.viewerName || null,
+    mergeDetails: (p) => resolveTableMergeDetails(p),
     isViewingLocked: (p) => {
       const exp = p?.viewLock?.expiresAt ? new Date(p.viewLock.expiresAt) : null;
       return !!(exp && exp > new Date());
