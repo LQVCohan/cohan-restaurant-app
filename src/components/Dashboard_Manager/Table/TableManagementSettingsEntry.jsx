519fb63067cc7f7d36eed09f55bf770a871d235f
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-11 03:26:32 +0700

    Move table settings action into page header

--- src/components/Dashboard_Manager/Table/TableManagementSettingsEntry.jsx
+++ src/components/Dashboard_Manager/Table/TableManagementSettingsEntry.jsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
 import { Tags } from "lucide-react";
 import TableManagement from "./TableManagement";
 import "./TableManagementSettingsEntry.scss";
 import "./TableTypeManagementMobileFix.scss";
 
-const TableManagementSettingsEntry = ({ onOpenTableSettings }) => (
-  <div className="tm-settings-page">
-    {onOpenTableSettings && (
-      <div className="tm-settings-page__toolbar" aria-label="Thiết lập bàn ăn">
-        <button
-          type="button"
-          className="tm-settings-page__button"
-          onClick={onOpenTableSettings}
-        >
-          <Tags size={18} aria-hidden="true" />
-          <span>Loại bàn &amp; không gian</span>
-        </button>
-      </div>
-    )}
-    <TableManagement />
-  </div>
-);
+const TableManagementSettingsEntry = ({ onOpenTableSettings }) => {
+  const rootRef = useRef(null);
+  const [controlsTarget, setControlsTarget] = useState(null);
+
+  useLayoutEffect(() => {
+    setControlsTarget(
+      onOpenTableSettings
+        ? rootRef.current?.querySelector(
+            ".management-page-header .mph-controls-row",
+          ) || null
+        : null,
+    );
+  }, [onOpenTableSettings]);
+
+  return (
+    <div className="tm-settings-page" ref={rootRef}>
+      <TableManagement />
+      {onOpenTableSettings &&
+        controlsTarget &&
+        createPortal(
+          <button
+            type="button"
+            className="mph-btn mph-btn--secondary tm-settings-page__button"
+            onClick={onOpenTableSettings}
+            title="Loại bàn & không gian"
+            aria-label="Loại bàn & không gian"
+          >
+            <Tags size={17} aria-hidden="true" />
+            <span>Loại bàn &amp; không gian</span>
+          </button>,
+          controlsTarget,
+        )}
+    </div>
+  );
+};
 
 export default TableManagementSettingsEntry;
