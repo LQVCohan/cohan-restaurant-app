e143b7e5dcfbd3b6e91f91c13b9ca46ab398d2e3
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-11 04:11:57 +0700

    Use existing testing library dependency

--- src/utils/installTableDetailModalTabs.test.js
+++ src/utils/installTableDetailModalTabs.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, screen } from "@testing-library/dom";
+import { fireEvent, screen } from "@testing-library/react";
 import { beforeEach, describe, expect, it } from "vitest";
 import { enhanceTableDetailModal } from "./installTableDetailModalTabs";
 
