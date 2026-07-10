4bb7e3b3445fe85f5e22733a48c62c3302a21b33
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-10 12:00:10 +0700

    feat(table): add in-page settings modal trigger

---
+++ src/components/Dashboard_Manager/Table/TableManagementSettingsEntry.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Tags } from "lucide-react";
+import TableManagement from "./TableManagement";
+import "./TableManagementSettingsEntry.scss";
+
+const TableManagementSettingsEntry = ({ onOpenTableSettings }) => (
+  <div className="tm-settings-page">
+    {onOpenTableSettings && (
+      <div className="tm-settings-page__toolbar" aria-label="Thiết lập bàn ăn">
+        <button
+          type="button"
+          className="tm-settings-page__button"
+          onClick={onOpenTableSettings}
+        >
+          <Tags size={18} aria-hidden="true" />
+          <span>Loại bàn &amp; không gian</span>
+        </button>
+      </div>
+    )}
+    <TableManagement />
+  </div>
+);
+
+export default TableManagementSettingsEntry;
