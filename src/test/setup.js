a2224ae19b309558db6400e4fcd710c6e6b1ed24
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-10 23:01:36 +0700

    test(setup): install app performance formatters

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,4 +1,5 @@
 import '@testing-library/jest-dom/vitest';
+import '@/utils/staffPerformanceGlobalFormat.js';
 import { vi } from 'vitest';
 
 vi.stubEnv('VITE_API_URL', 'http://localhost:4000/graphql');
