71fcef94b8ee92ba872882e68d2b11dc054cf6cd
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-10 21:07:42 +0700

    feat(qr-order): compose table ordering and proof updates

---
+++ src/components/Customer/TableCurrentSession/TableOrderRouteExperience.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import TableOrderExperience from "./TableOrderExperience";
+import TableProofUpdateNotifier from "./TableProofUpdateNotifier";
+
+export default function TableOrderRouteExperience() {
+  return (
+    <>
+      <TableOrderExperience />
+      <TableProofUpdateNotifier />
+    </>
+  );
+}
