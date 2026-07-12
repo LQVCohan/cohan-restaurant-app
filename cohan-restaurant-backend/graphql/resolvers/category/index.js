d6017e5ef26b09f1dc76b641008624c09ada6896
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-12 08:38:36 +0700

    feat(menu): activate exact menu category routing

--- cohan-restaurant-backend/graphql/resolvers/category/index.js
+++ cohan-restaurant-backend/graphql/resolvers/category/index.js
@@ -1,5 +1,6 @@
 import { CategoryQuery } from "./query.js";
 import { CustomerCategoryQuery } from "./customerQuery.js";
+import { CategoryMultiSlotQuery } from "./multiSlotQuery.js";
 import { CategoryMutation } from "./mutation.js";
 import Category from "./types.js";
 
@@ -7,6 +8,7 @@ export default {
   Query: {
     ...CategoryQuery,
     ...CustomerCategoryQuery,
+    ...CategoryMultiSlotQuery,
   },
   Mutation: { ...CategoryMutation },
   ...Category,
