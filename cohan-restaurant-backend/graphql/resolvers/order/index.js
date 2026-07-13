740c479c38862c59fb57a7783af417ea23df03b8
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-14 01:36:02 +0700

    feat(table): connect pre-session customer request lifecycle

--- cohan-restaurant-backend/graphql/resolvers/order/index.js
+++ cohan-restaurant-backend/graphql/resolvers/order/index.js
@@ -20,11 +20,16 @@ import { withCheckoutIdempotency } from "./checkoutIdempotency.js";
 import { withCheckoutContactGuard } from "./checkoutContactGuard.js";
 import { withDeferredOnlineCheckout } from "./deferredOnlineCheckout.js";
 import publicTableSessionQuery from "./publicTableSessionQuery.js";
+import publicTableSessionQueryEnhanced from "./publicTableSessionQueryEnhanced.js";
 import publicTableOrderMutation from "./publicTableOrderMutation.js";
 import {
   PublicTableOrderAccessMutation,
   PublicTableOrderAccessQuery,
 } from "./publicTableOrderAccess.js";
+import {
+  withTableCustomerRequestMutations,
+  withTableCustomerRequestQuery,
+} from "./tableCustomerRequestBridge.js";
 
 const PaymentGuardedOrderMutation = {
   ...OrderMutation,
@@ -42,7 +47,6 @@ const LifecycleOrderMutation = withTablePaymentRequestLifecycle(
 const HardenedOrderMutation = withOrderConflictHardening(LifecycleOrderMutation);
 const CanonicalCheckoutOrderMutation = {
   ...HardenedOrderMutation,
-  // ponytail: the canonical checkout resolver owns cart-hold validation and release.
   createCheckoutOrders: LifecycleOrderMutation.createCheckoutOrders,
 };
 const DeferredOnlineOrderMutation = withDeferredOnlineCheckout(
@@ -55,20 +59,23 @@ const CheckoutSafeOrderMutation = withCheckoutIdempotency(
   ContactGuardedOrderMutation,
 );
 const GuardedOrderMutation = withOrderRestaurantAccessGuards(CheckoutSafeOrderMutation);
+const TableRequestOrderMutation = withTableCustomerRequestMutations(GuardedOrderMutation);
 
 const CanonicalOrderQuery = { ...OrderQuery };
 delete CanonicalOrderQuery.managerDashboard;
+const TableRequestOrderQuery = withTableCustomerRequestQuery(CanonicalOrderQuery);
 
 export default {
   Query: {
-    ...CanonicalOrderQuery,
+    ...TableRequestOrderQuery,
     ...OrderCoreRecoveryQuery,
     ...publicTableSessionQuery,
+    ...publicTableSessionQueryEnhanced,
     ...CustomerOrderHistoryQuery,
     ...PublicTableOrderAccessQuery,
   },
   Mutation: {
-    ...GuardedOrderMutation,
+    ...TableRequestOrderMutation,
     ...OrderProofMutation,
     ...CustomerOrderHistoryMutation,
     ...publicTableOrderMutation,
