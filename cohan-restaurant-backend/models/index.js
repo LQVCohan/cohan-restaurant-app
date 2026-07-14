4e5ab3103a92bca8c8576b3897b3bb62a5648b25
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-14 10:32:02 +0700

    fix(models): restore voucher package export path

--- cohan-restaurant-backend/models/index.js
+++ cohan-restaurant-backend/models/index.js
@@ -53,7 +53,7 @@ export { default as TableEvent } from "./table-event.model.js";
 export { default as Coupon } from "./coupon.model.js";
 export { default as CouponRedemption } from "./coupon-redemption.model.js";
 export { default as UserCoupon } from "./user-coupon.model.js";
-export { default as VoucherPackage } from "./voucherPackage.model.js";
+export { default as VoucherPackage } from "./voucher-package.model.js";
 export { default as Shift } from "./shift.model.js";
 export { default as Timesheet } from "./timesheet.model.js";
 export { default as LeaveRequest } from "./leave-request.model.js";
