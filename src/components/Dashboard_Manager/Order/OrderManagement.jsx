03339c213e93999995f04bac988640d60d9d1694
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-14 09:51:09 +0700

    fix(order): preserve named exports from order management

--- src/components/Dashboard_Manager/Order/OrderManagement.jsx
+++ src/components/Dashboard_Manager/Order/OrderManagement.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import OrderManagementCore from "./OrderManagementCore";
 import OrderFutureOrdersDock from "./components/OrderFutureOrdersDock";
 
+export * from "./OrderManagementCore";
+
 export default function OrderManagement() {
   return (
     <>
