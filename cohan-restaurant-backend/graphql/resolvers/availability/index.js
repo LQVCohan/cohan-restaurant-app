c4cda16e87e08e6f129f221d1b87f1fab936d4c2
LQVCohan <112467167+LQVCohan@users.noreply.github.com>
2026-07-17 16:59:13 +0700

    feat(availability): wire workspace-aware resolvers

--- cohan-restaurant-backend/graphql/resolvers/availability/index.js
+++ cohan-restaurant-backend/graphql/resolvers/availability/index.js
@@ -1,18 +1,27 @@
-import Query from "./query.js";
+import BaseQuery from "./query.js";
 import BaseMutation from "./mutation.js";
 import WindowLifecycleMutation from "./windowLifecycle.mutation.js";
 import { resolveAvailabilityWindowEffectiveStatus } from "../../../src/services/availability/availabilityRegistrationSchedule.service.js";
+import {
+  normalizeAvailabilityWorkspaceType,
+  withAvailabilityWorkspaceMutations,
+  withAvailabilityWorkspaceQueries,
+} from "./workspaceScope.js";
 
-const Mutation = {
+const Query = withAvailabilityWorkspaceQueries(BaseQuery);
+
+const Mutation = withAvailabilityWorkspaceMutations({
   ...BaseMutation,
   ...WindowLifecycleMutation,
-};
+});
 
 const AvailabilityWindow = {
   effectiveStatus: (windowDoc) =>
     resolveAvailabilityWindowEffectiveStatus(windowDoc),
   registrationMode: (windowDoc) =>
     String(windowDoc?.registrationModeSnapshot || "manual"),
+  workspaceType: (windowDoc) =>
+    normalizeAvailabilityWorkspaceType(windowDoc?.workspaceType),
 };
 
 export default { Query, Mutation, AvailabilityWindow };
